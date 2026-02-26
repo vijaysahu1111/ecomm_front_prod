356aa0b6de8ceb264e573ac0d7816d577e152ee4
vijaysahu1111 <vijaysahumodi1111@gmail.com>
2026-02-26 15:16:48 +0530

    change in db

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
 // import { DB_URL } from '../config/config.js'
 let DB_URL = process.env.DB_URL;
+import dns from 'node:dns/promises'
+dns.setServers(["8.8.8.8","1.1.1.1"]);
+
 
 mongoose
 .connect(DB_URL)
